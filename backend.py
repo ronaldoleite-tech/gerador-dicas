09a1c35aeea761b64463cfcae4a8b2de45271384
ronaldoleite-tech <ronaldoleite75@gmail.com>
2025-10-20 16:25:29 -0300

    Atualização 20/10 3

--- backend.py
+++ backend.py
@@ -535,7 +535,15 @@ def get_todos_resultados():
 
 @app.route('/landing')
 def promocional():
-    return render_template('landing.html')    
+    return render_template('landing.html')
+
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')    
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
